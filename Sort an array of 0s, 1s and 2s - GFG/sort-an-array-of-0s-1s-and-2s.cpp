89c99dc769eef3966a7d53cee4cd34794a897179
Riyansh Garg <riyanshgarg2103@gmail.com>
2022-12-27 02:13:53 +0530

    Added solution - LeetHub

--- Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.cpp
+++ Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.cpp
@@ -10,7 +10,34 @@ class Solution
     public:
     void sort012(int a[], int n)
     {
-        sort(a,a+n);
+        // Method 1 : Dirrect sorting
+        // sort(a,a+n);
+        
+        // Method 2: using maps; // last case TLE
+        // unordered_map<int,int> m;
+        // for(int i=0;i<n;i++){
+        //     m[a[i]]++;
+        // }
+        // for(int i=0;i<n;i++){
+        //     if(m[0]!=0){a[i]=0;m[0]--;}
+        //     else if(m[1]!=0){a[i]=1;m[1]--;}
+        //     else{a[i]=2;m[2]--;}
+        // }
+        
+        // Method 3 using dutch national flag algorithm
+        
+        int low=0,mid=0,high=n-1;
+        for(int i=0;i<n;i++){
+            if(a[mid]==0){
+                swap(a[mid],a[low]);
+                low++;mid++;
+            }
+            else if(a[mid]==1)   mid++;
+            else{
+                swap(a[mid],a[high]);
+                high--;
+            }
+        }
     }
     
 };
