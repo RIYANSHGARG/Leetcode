dc4066646ed0246ac79f599e8e98a2864346e2f1
Riyansh Garg <riyanshgarg2103@gmail.com>
2023-01-02 13:20:00 +0530

    Added solution - LeetHub

--- Common elements - GFG/common-elements.cpp
+++ Common elements - GFG/common-elements.cpp
@@ -4,31 +4,51 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution
 {
     public:    
        vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
         {
-            vector<int> ans;
-            unordered_set<int> m1;
-            unordered_set<int> m2;
-            unordered_set<int> m3;
-            for(int i=0;i<n1;i++)   {m1.insert(A[i]);}
-            for(int i=0;i<n2;i++)   {m2.insert(B[i]);}
-            for(int i=0;i<n3;i++)   {m3.insert(C[i]);}
-            for(int i=0;i<n1;i++){
-                if(m1.find(A[i])!=m1.end()&&m2.find(A[i])!=m2.end()&&m3.find(A[i])!=m3.end()){
-                    ans.push_back(A[i]);
-                    m1.erase(A[i]);
-                }
-            }
-            return ans;
+            // vector<int> ans;
+            // unordered_set<int> m1;
+            // unordered_set<int> m2;
+            // unordered_set<int> m3;
+            // for(int i=0;i<n1;i++)   {m1.insert(A[i]);}
+            // for(int i=0;i<n2;i++)   {m2.insert(B[i]);}
+            // for(int i=0;i<n3;i++)   {m3.insert(C[i]);}
+            // for(int i=0;i<n1;i++){
+            //     if(m1.find(A[i])!=m1.end()&&m2.find(A[i])!=m2.end()&&m3.find(A[i])!=m3.end()){
+            //         ans.push_back(A[i]);
+            //         m1.erase(A[i]);
+            //     }
+            // }
+            // return ans;
             
             // Method 2
-            // int i=0,j=0,k=0;
-            // while(i<n1&&j<n2&&k<n3){
-                
-            // }
+            vector<int> ans;
+            int i=0,j=0,k=0;
+            while(i<n1&&j<n2&&k<n3){
+                if(A[i]==B[j]&&B[j]==C[k]){
+                    ans.push_back(A[i]);
+                    i++;j++;k++;
+                }
+                else if(A[i]<B[j]){
+                    i++;
+                }else if(B[j]<C[k]){
+                    j++;                   
+                }else{
+                    k++;
+                }
+                int a=A[i-1];
+                while(a==A[i]){i++;}
+                int b=B[j-1];
+                while(b==B[j]){j++;}
+                // int c=C[k-1];
+                // while(c==C[i]){k++;}
+            }
+            if(ans.size()==0)   return {-1};
+            else    return ans;
         }
 
 };
