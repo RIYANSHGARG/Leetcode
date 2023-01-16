c3443d369e50bb757d7bc568aa4b41a1df179433
Riyansh Garg <riyanshgarg2103@gmail.com>
2023-01-16 13:42:49 +0530

    Added solution - LeetHub

--- Smallest subarray with sum greater than x - GFG/smallest-subarray-with-sum-greater-than-x.cpp
+++ Smallest subarray with sum greater than x - GFG/smallest-subarray-with-sum-greater-than-x.cpp
@@ -10,13 +10,28 @@ class Solution{
     int smallestSubWithSum(int arr[], int n, int x)
     {
         // Mehthod 1 : O(N^2)
-        int ans=INT_MAX,sum=0,count=0;
-        for(int i=0;i<n;i++){
-            sum=arr[i];count=1;
-            if(arr[i]>x)   return 1;
-            for(int j=i+1;j<n;j++){
-                sum+=arr[j];count++;
-                if(sum>x)  {ans=min(count,ans);break;}
+        // int ans=INT_MAX,sum=0,count=0;
+        // for(int i=0;i<n;i++){
+        //     sum=arr[i];count=1;
+        //     if(arr[i]>x)   return 1;
+        //     for(int j=i+1;j<n;j++){
+        //         sum+=arr[j];count++;
+        //         if(sum>x)  {ans=min(count,ans);break;}
+        //     }
+        // }
+        // return (ans<INT_MAX)?ans:0;
+        
+        
+        // method 2 : Variable lengthed sliding window. 
+        
+        int ans=INT_MAX,i=0,j=0,sum=arr[j];
+        while(i<n&&j<n){
+            if(sum>x){
+                ans=min(ans,j-i+1);
+                sum-=arr[i];
+                i++;
+            }else{
+                sum+=arr[++j];
             }
         }
         return (ans<INT_MAX)?ans:0;
