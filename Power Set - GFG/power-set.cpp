e9cecfa92b9cd6cc3a055510418bb470fd165998
Riyansh Garg <riyanshgarg2103@gmail.com>
2023-02-07 18:12:06 +0530

    Added solution - LeetHub

--- Power Set - GFG/power-set.cpp
+++ Power Set - GFG/power-set.cpp
@@ -8,17 +8,37 @@ using namespace std;
 using namespace std;
 class Solution{
 	public:
+	
+	    void recursion(string s,int i,vector<string> &ans,string temp){
+	        if(i==s.length()){
+	            if(temp=="")    return ;
+	            ans.push_back(temp);
+	            return ;
+	        }
+	        temp+=s[i];
+	        recursion(s,i+1,ans,temp);
+	        temp.pop_back();
+	        recursion(s,i+1,ans,temp);
+	    }
+	
 		vector<string> AllPossibleStrings(string s){
-		    vector<string> ans;
-		    for(int i=1;i<(1<<s.length());i++){
-		        string temp="";
-		        for(int j=0;j<s.length();j++){
-		            if(i&(1<<j))    temp+=s[j];
-		        }
-		        ans.push_back(temp);
-		    }
-		    sort(ans.begin(),ans.end());
-		    return ans;
+		  //  vector<string> ans;
+		  //  for(int i=1;i<(1<<s.length());i++){
+		  //      string temp="";
+		  //      for(int j=0;j<s.length();j++){
+		  //          if(i&(1<<j))    temp+=s[j];
+		  //      }
+		  //      ans.push_back(temp);
+		  //  }
+		  //  sort(ans.begin(),ans.end());
+		  //  return ans;
+		  
+		  // Method 2 : Recursion
+		  string temp;
+		  vector<string> ans;
+		  recursion(s,0,ans,temp);
+		  sort(ans.begin(),ans.end());
+		  return ans;
 		}
 };
 
