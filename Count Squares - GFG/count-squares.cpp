66a6894b2733370f2259e33475b8cf7b933d08c3
Riyansh Garg <riyanshgarg2103@gmail.com>
2023-02-12 04:23:59 +0530

    Added solution - LeetHub

--- Count Squares - GFG/count-squares.cpp
+++ Count Squares - GFG/count-squares.cpp
@@ -7,7 +7,15 @@ class Solution {
   public:
     int countSquares(int N) {
         // code here
-        return int(sqrt(N-1));
+        // return int(sqrt(N-1));
+        
+        long long low=1,high=N;
+        while(low<=high){
+            long long  mid=(low+high)/2;
+            if(mid*mid > N) high=mid-1;
+            else    low=mid+1;
+        }
+        return (high*high==N)?high-1:high;
     }
 };
 
