4c5b75570a87fb0edde847c01f1d14bba257402f
Riyansh Garg <riyanshgarg2103@gmail.com>
2023-04-12 19:46:03 +0530

    Time: 6 ms (63.19%), Space: 10.5 MB (46.42%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string simplifyPath(string s) {
+        stack<string> st;
+        for(int i=0;i<s.length();++i){
+            if(s[i]=='/')   continue;
+            
+            string temp="";
+            while(i<s.length() && s[i]!='/'){
+                temp+=s[i];
+                ++i;
+            }
+            
+            if(temp==".")   continue;
+            else if(temp==".."){
+                if(!st.empty()) st.pop();
+            }
+            else    st.push(temp);   
+        }
+        
+        string ans="";
+        while(!st.empty())  {ans="/"+st.top()+ans;st.pop();}
+        if(ans.size()==0)   return "/";
+        return ans;
+    }
+};
