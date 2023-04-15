5bc907ebced25695919c42391d2541664a5704d7
Riyansh Garg <riyanshgarg2103@gmail.com>
2023-04-15 16:54:39 +0530

    Time: 48 ms (22.93%), Space: 18.4 MB (21.70%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool valid(int newrow,int newcol,vector<vector<char>> &grid, vector<vector<bool>> &vis){
         if(newrow>=0 && newrow <grid.size() && newcol>=0 && newcol <grid[0].size() && grid[newrow][newcol]=='1' && vis[newrow][newcol]==false){
+            vis[newrow][newcol]=true;
             return true;
         }
         return false;
@@ -15,26 +16,14 @@ public:
             int i=q.front().first,j=q.front().second;
             q.pop();
             
-            // Best method to traverse every neighbours 
             
-            if(valid(i-1,j,grid,vis)){
-                vis[i-1][j]=true;
-                q.push({i-1,j});
-            }
-            if(valid(i,j-1,grid,vis)){
-                vis[i][j-1]=true;
-                q.push({i,j-1});
-            }
-            if(valid(i,j+1,grid,vis)){
-                vis[i][j+1]=true;
-                q.push({i,j+1});
-            }
-            if(valid(i+1,j,grid,vis)){
-                vis[i+1][j]=true;
-                q.push({i+1,j});
-            }
+            if(valid(i-1,j,grid,vis))   q.push({i-1,j});
+            if(valid(i,j-1,grid,vis))   q.push({i,j-1});
+            if(valid(i,j+1,grid,vis))   q.push({i,j+1});
+            if(valid(i+1,j,grid,vis))   q.push({i+1,j});
         }
     }
+    
     int numIslands(vector<vector<char>>& grid) {
         int n=grid.size(),m=grid[0].size(),count=0;
         vector<vector<bool>> vis(n,vector<bool>(m,false));
@@ -42,18 +31,11 @@ public:
             for(int j=0;j<m;j++){
                 
                 if(grid[i][j]=='1' && vis[i][j]==false){
-                    cout<<i<<" "<<j<<endl;
                     bfs(grid,i,j,vis);
                     count++;
                 }
             }
         }
-        
-        for(auto i:vis){
-            for(auto j:i)   cout<<j<<" ";
-            cout<<endl;
-        }
-        
         return count;
     }
 };
