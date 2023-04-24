bf9e50aed99682820e2d9b0b171310e51e9b3c21
Riyansh Garg <riyanshgarg2103@gmail.com>
2023-04-24 19:39:51 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -16,16 +16,48 @@ class Solution
 	}
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
-	    stack<int> s;
-	    vector<int> vis(V,0),ans;
+	    
+	    // Method 1 : topological sort
+	    // if there is any edge between u and v then always u will appears before u and v in a linear order.
+	    // only valid in DAG (directed and acyclic graphs)
+	    
+	    // stack<int> s;
+	    // vector<int> vis(V,0),ans;
+	    // for(int i=0;i<V;i++){
+	    //     if(!vis[i]) dfs(i,vis,adj,s);
+	    // }
+	    // while(!s.empty()){
+	    //     ans.push_back(s.top());
+	    //     s.pop();
+	    // }
+	    // return ans;
+	   
+	    // Method 2 : Kahn's Algorithm (Using BFS)
+	    // Indegree -> number of incoming edges for a node
+	    // inserting all nodes with indegree 0
+	    
+	    // calculating indegrees
+	    vector<int> indeg(V,0);
 	    for(int i=0;i<V;i++){
-	        if(!vis[i]) dfs(i,vis,adj,s);
+	        for(auto j:adj[i]){
+	            indeg[j]++;
+	        }
 	    }
-	    while(!s.empty()){
-	        ans.push_back(s.top());
-	        s.pop();
+	    queue<int> q;
+	    vector<int> topo;
+	    for(int i=0;i<indeg.size();i++){
+	        if(indeg[i]==0) q.push(i);
 	    }
-	    return ans;
+	    while(!q.empty()){
+	        int node=q.front();
+	        topo.push_back(node);
+	        q.pop();
+	        for(auto i:adj[node]){
+	            indeg[i]--;
+	            if(indeg[i]==0) q.push(i);
+	        }
+	    }
+	    return topo;
 	}
 };
 
