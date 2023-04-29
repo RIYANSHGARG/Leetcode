19008b3743bc3ceb27e7fa5388ba0dd647c57bed
Riyansh Garg <riyanshgarg2103@gmail.com>
2023-04-29 15:45:33 +0530

    Added solution - LeetHub

--- Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
+++ Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
@@ -10,23 +10,47 @@ class Solution
     //from the source vertex S.
     vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
     {
-        vector<int> dist(V,INT_MAX);
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> q;
-        q.push({0,S});
-        dist[S]=0;
-        while(!q.empty()){
-            int step=q.top().first;
-            int node = q.top().second;
-            q.pop();
+        // TC : ElogV
+        //      E is total number of edges V i total no of nodes
+        // Priority queue is used because we always want ot work on minim un distance first
+        // vector<int> dist(V,INT_MAX);
+        // min heap
+    //     priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> q;
+    //     q.push({0,S});
+    //     dist[S]=0;
+    //     while(!q.empty()){
+    //         int dis=q.top().first;
+    //         int node = q.top().second;
+    //         q.pop();
             
-            for(auto i:adj[node]){
-                if(dist[i[0]] > step+i[1]){
-                    dist[i[0]]=step+i[1];
-                    q.push({step+i[1],i[0]});
-                }
+    //         for(auto i:adj[node]){
+    //             if(dist[i[0]] > dis+i[1]){
+    //                 dist[i[0]]=dis+i[1];
+    //                 q.push({dis+i[1],i[0]});
+    //             }
+    //         }
+    //     }
+    //     return dist;
+    
+    // Method 2 : using Set
+    set<pair<int,int>> s;
+    s.insert({0,S});
+    vector<int> dist(V,INT_MAX);
+    dist[S]=0;
+    while(!s.empty()){
+        auto it=*(s.begin());
+        int dis=it.first;
+        int node=it.second;
+        s.erase(it);
+        
+        for(auto i:adj[node]){
+            if(dis+i[1] < dist[i[0]]){
+                dist[i[0]]=dis+i[1];
+                s.insert({dis+i[1],i[0]});
             }
         }
-        return dist;
+    }
+    return dist;
     }
 };
 
