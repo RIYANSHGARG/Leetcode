d2fb1d5c8607eb81868a65b0262438fae9800d32
Riyansh Garg <riyanshgarg2103@gmail.com>
2023-08-20 18:36:43 +0530

    Time: 62 ms (52.06%), Space: 24.7 MB (12.00%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,40 +1,17 @@
 class Solution {
 public:
-    int rec(int idx1,int idx2, string &s1,string &s2, vector<vector<int>> &dp){
-        if(idx1<0 || idx2<0)    return 0;
-        if(dp[idx1][idx2]!=-1)  return dp[idx1][idx2];
-        if(s1[idx1]==s2[idx2])  return dp[idx1][idx2]=1+rec(idx1-1,idx2-1,s1,s2,dp);
-        return dp[idx1][idx2]=0+max(rec(idx1-1,idx2,s1,s2,dp),rec(idx1,idx2-1,s1,s2,dp));
+    
+    int rec(string &s1,int i1, string &s2, int i2,  vector<vector<int>> &dp){
+        if(i1<0 || i2<0)    return 0;
+        if(dp[i1][i2]!=-1)  return dp[i1][i2];
+        if(s1[i1] == s2[i2])    return dp[i1][i2] = 1+rec(s1,i1-1,s2,i2-1,dp);
+        return dp[i1][i2] = max(rec(s1,i1-1,s2,i2,dp),rec(s1,i1,s2,i2-1,dp));
     }
     
     int longestCommonSubsequence(string text1, string text2) {
-        // vector<vector<int>> dp(text1.length(),vector<int> (text2.length(),-1));
-        // return rec(text1.length()-1,text2.length()-1,text1,text2,dp);
-        int n=text1.length(),m=text2.length();
-        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
-        // for(int i=0;i<text1.length();i++)   dp[]
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<m+1;j++){
-                if(text1[i-1]==text2[j-1])  dp[i][j]=1+dp[i-1][j-1];
-                else    dp[i][j]=0+max(dp[i-1][j],dp[i][j-1]);
-            }
-        }
-        // return dp[n][m];
         
-        // Printing LCS : Traversing dp matrix in reverse to get elements.
-        string ans;
-        int i=n,j=m;
-        while(i>0 && j >0){
-            if(text1[i-1] == text2[j-1])    {ans+=text1[i-1];i--;j--;}
-            else{
-                if(dp[i-1][j] > dp[i][j-1]){
-                    i--;
-                }else{j--;}
-            }
-        }
-        
-        reverse(ans.begin(),ans.end());
-        cout<<ans;
-        return dp[n][m];
+        vector<vector<int>> dp(text1.length(),vector<int> (text2.length(),-1));
+        int ans=rec(text1,text1.length()-1, text2, text2.length()-1,dp);
+        return ans;
     }
 };
