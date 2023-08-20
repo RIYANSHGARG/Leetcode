6466c4c4aacfd7683d822f0e6169724565a9c56c
Riyansh Garg <riyanshgarg2103@gmail.com>
2023-08-20 18:19:44 +0530

    Added solution - LeetHub

---
+++ Reverse Bits - GFG/reverse-bits.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long long reversedBits(long long X) {
+        long long result= 0;
+        for(int i=0; i<32; i++)
+            result = (result<<1) + (X>>i &1);
+        
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long X;
+        
+        cin>>X;
+
+        Solution ob;
+        cout << ob.reversedBits(X) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
