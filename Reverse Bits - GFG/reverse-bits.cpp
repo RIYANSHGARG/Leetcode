b45b61722be70e17ded79404319ad82a1d81bfb4
Riyansh Garg <riyanshgarg2103@gmail.com>
2023-08-20 18:21:35 +0530

    Added solution - LeetHub

--- Reverse Bits - GFG/reverse-bits.cpp
+++ Reverse Bits - GFG/reverse-bits.cpp
@@ -6,11 +6,28 @@ using namespace std;
 class Solution {
   public:
     long long reversedBits(long long X) {
-        long long result= 0;
-        for(int i=0; i<32; i++)
-            result = (result<<1) + (X>>i &1);
         
-        return result;
+        int count=32;
+        string s="";
+        while(count--){
+            if(X&1){s+="1";}
+            else{s+="0";}
+            X>>=1;
+        }
+        // cout<<s;
+        long long int ans=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='1'){
+                ans+=pow(2,(31-i) );    
+            }
+        }
+        return ans;
+        
+        // long long result= 0;
+        // for(int i=0; i<32; i++)
+        //     result = (result<<1) + (X>>i &1);
+        
+        // return result;
     }
 };
 
