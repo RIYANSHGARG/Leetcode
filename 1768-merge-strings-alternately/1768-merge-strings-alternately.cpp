347572f72988d62b20e09080481b435bd043601c
Riyansh Garg <riyanshgarg2103@gmail.com>
2023-09-01 23:02:08 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (45.16%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    string getString(char x)
-    {
-        string s(1, x);
-        return s;  
-    }
     string mergeAlternately(string word1, string word2) {
         int i=0,j=0;
-        string ans;
+        string ans="";
         while(i<word1.length() && j<word2.length()){
-            ans+=getString(word1[i])+getString(word2[j]);
+            ans+=word1[i];
+            ans+=word2[j];
             i++;j++;
         }
-        while(i<word1.length()) ans+=word1[i++];
-        while(j<word2.length()) ans+=word2[j++];
+        while(i<word1.length()){
+            ans+=word1[i];
+            i++;
+        }
+        while(j<word2.length()){
+            ans+=word2[j];
+            j++;
+        }
         return ans;
     }
 };
