a1f31f9e53c6cdd8d8ddf62bc0580f293d6306d1
Riyansh Garg <riyanshgarg2103@gmail.com>
2023-09-21 04:16:00 +0530

    Time: 380 ms (34.15%), Space: 90.4 MB (25.28%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -5,13 +5,19 @@ public:
         sort(intervals.begin(),intervals.end());
         stack<pair<int,int>> s;
         s.push({intervals[0][0] , intervals[0][1]});
+        // Total 6 possibilities
         for(int i=1;i<intervals.size();i++){
+            // it contains 1 possibilities
             if(intervals[i][0] < s.top().second && intervals[i][1] > s.top().second)  count++;
+            
+            // it contains 3 possibilities
             else if(intervals[i][0] >= s.top().first && intervals[i][1] <= s.top().second){
                 count++;
                 s.pop();
                 s.push({intervals[i][0] , intervals[i][1]});
             }
+            
+            // it contains 2 possibilities
             else    s.push({intervals[i][0] , intervals[i][1]});
         }
         return count;
