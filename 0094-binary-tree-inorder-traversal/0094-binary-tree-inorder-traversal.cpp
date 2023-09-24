c0264fe9a76d3961cd9e4e9c2eabba4c0f01e2f4
Riyansh Garg <riyanshgarg2103@gmail.com>
2023-09-24 20:58:59 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (16.27%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -93,21 +93,47 @@ vector<int> v;
         // return v;
         
         //  iterative
-        TreeNode *node = root;
-        stack<TreeNode *> s;
-        vector<int> in;
-        while(true){
-            if(node){
-                s.push(node);
-                node=node->left;
-            }else{
-                if(s.empty())   break;
-                node = s.top();
-                s.pop();
+        // TreeNode *node = root;
+        // stack<TreeNode *> s;
+        // vector<int> in;
+        // while(true){
+        //     if(node){
+        //         s.push(node);
+        //         node=node->left;
+        //     }else{
+        //         if(s.empty())   break;
+        //         node = s.top();
+        //         s.pop();
+        //         in.push_back(node->val);
+        //         node=node->right;
+        //     }
+        // }
+        // return in;
+        
+        //  All in one stack
+        if(root==NULL)  return {};
+        stack<pair<TreeNode *,int>> s;
+        vector<int> pre,in,post;
+        
+        s.push({root,1});
+        while(!s.empty()){
+            TreeNode *node = s.top().first;
+            int l=s.top().second;
+            s.pop();
+            if(l==1){
+                pre.push_back(node->val);
+                s.push({node,2});
+                if(node->left)  s.push({node->left,1});
+            }
+            else if(l==2){
                 in.push_back(node->val);
-                node=node->right;
+                s.push({node,3});
+                if(node->right) s.push({node->right,1});
+            }else{
+                post.push_back(node->val);
             }
         }
         return in;
+        
     }
 };
