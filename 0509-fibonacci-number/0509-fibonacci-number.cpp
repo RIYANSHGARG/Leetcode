9f8f87745ad3c6df7aa70d8a019aea6e7a5b7596
Riyansh Garg <riyanshgarg2103@gmail.com>
2024-02-01 01:07:13 +0530

    Time: 2 ms (76.73%), Space: 7.2 MB (7.81%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    int rec(int i){
-        if(i==0 || i==1)    return i;
-        
-        return rec(i-1)+rec(i-2);
+    int rec(int i,vector<int> &dp){
+        if(i==0 || i==1)    return dp[i]=i;
+        if(dp[i]!=-1)   return dp[i];
+        return dp[i]=rec(i-1,dp)+rec(i-2,dp);
     }
     int fib(int n) {
-        int ans=rec(n);
+        vector<int> dp(n+1,-1);
+        int ans=rec(n,dp);
         return ans;
     }
 };
