a32fad2d31293e41769faef834bf150111a97a89
Riyansh Garg <riyanshgarg2103@gmail.com>
2024-02-01 19:47:27 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (11.88%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -17,6 +17,24 @@ public:
     
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m,vector<int> (n,-1));
-        return rec(m-1,n-1,dp);
+        // return rec(m-1,n-1,dp);
+        dp[0][0]=1;
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0&&j==0)  continue;
+                int left=0,right=0;
+                if(i>0)  left=dp[i-1][j]%mod;
+                if(j>0)  right=dp[i][j-1]%mod ;
+                dp[i][j]=(left+right)%mod;
+            }
+        }
+        
+        
+        
+        
+        return dp[m-1][n-1];
+        
+        
     }
 };
