ffb1ff7b04860c1655f9b8b826addf5c1b9db2d5
Riyansh Garg <riyanshgarg2103@gmail.com>
2024-02-01 17:36:14 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (5.70%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,22 @@
+int mod = (int)2*1e9;
+
+class Solution {
+public:
+    
+    int rec(int i,int j,vector<vector<int>> &dp){
+        if(i<0 || j<0)  return 0;
+        if(i==0 && j==0) return dp[i][j]=1%mod;
+         
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        int left = rec(i,j-1,dp)%mod;
+        int up = rec(i-1,j,dp)%mod;
+        
+        return dp[i][j]=(left%mod+up%mod)%mod;
+    }
+    
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int> (n,-1));
+        return rec(m-1,n-1,dp);
+    }
+};
