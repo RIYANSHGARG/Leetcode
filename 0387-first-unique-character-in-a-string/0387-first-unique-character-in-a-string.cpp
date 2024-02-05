f02f7e09f6fa6186e1574863b3ed2c6d1931e4b8
Riyansh Garg <riyanshgarg2103@gmail.com>
2024-02-05 20:52:00 +0530

    Time: 39 ms (32.76%), Space: 13 MB (17.78%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        map<char,int> m;
-        set<char> st;
+        // map<char,int> m;
+        // set<char> st;
+        // for(int i=0;i<s.length();i++){
+        //     if(st.find(s[i]) !=st.end() ){
+        //         continue;
+        //     }else if(m.find(s[i])!=m.end()){
+        //         st.insert(s[i]);
+        //         m.erase(s[i]);
+        //     }
+        //     else{
+        //         m[s[i]]=i;
+        //     }
+        // }
+        // if(m.size()==0) return -1;
+        // int ans=INT_MAX;
+        // for(auto i:m){
+        //     ans=min(ans,i.second);
+        // }
+        // return ans;
+        
+        unordered_map<char,int> m;
+        for(int i=0;i<s.length();i++)   m[s[i]]++;
         for(int i=0;i<s.length();i++){
-            if(st.find(s[i]) !=st.end() ){
-                continue;
-            }else if(m.find(s[i])!=m.end()){
-                st.insert(s[i]);
-                m.erase(s[i]);
-            }
-            else{
-                m[s[i]]=i;
-            }
+            if(m[s[i]]==1)  return i;
         }
-        if(m.size()==0) return -1;
-        int ans=INT_MAX;
-        for(auto i:m){
-            ans=min(ans,i.second);
-        }
-        return ans;
+        return -1;
     }
 };
