fd2dedaf0a4d93e85177abd7b7a885bf3a1cec1c
Riyansh Garg <riyanshgarg2103@gmail.com>
2024-04-03 12:02:25 +0530

    Time: 478 ms (20.55%), Space: 220.2 MB (30.38%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    
-    int rec(int i, int buy, int times , vector<int> &prices , vector<vector<vector<int>>> &dp){
+    int rec(int i, vector<int> &prices, int buy,vector<vector<vector<int>>> &dp,int count){
+        if(count>=2) return 0;
         if(i>=prices.size())    return 0;
-        if(times==0)    return 0;
         
-        if(dp[i][buy][times]!=-1)  return dp[i][buy][times];
+        if(dp[i][buy][count]!=-1)  return dp[i][buy][count];
         
-        int pick=INT_MIN,notPick=INT_MIN,profit;
-        if(buy){
-            pick=-prices[i]+rec(i+1,0,times,prices,dp);
-            notPick=rec(i+1,1,times,prices,dp);
-            profit = max(pick,notPick);
+        int pick=INT_MIN,notPick=INT_MIN,profit=0;
+        if(buy==0){
+            pick=-prices[i]+rec(i+1,prices,1,dp,count);
+            notPick=rec(i+1,prices,0,dp,count);
+            profit=max(pick,notPick);
         }else{
-            pick = prices[i] + rec(i+1,1,times-1,prices,dp);
-            notPick = rec(i+1,0,times,prices,dp);
-            profit = max(pick,notPick);
+            pick=prices[i]+rec(i+1,prices,0,dp,count+1);
+            notPick = rec(i+1,prices,1,dp,count);
+            profit=max(pick,notPick);
         }
-        return dp[i][buy][times]=profit;
+        return dp[i][buy][count]=profit;
+        
     }
-    
     int maxProfit(vector<int>& prices) {
-        vector<vector<vector<int>>> dp(prices.size(),vector<vector<int>> (2,vector<int> (3,-1)));
-        return rec(0,1,2,prices,dp);
+        vector<vector<vector<int>>> dp(prices.size(),vector<vector<int>> (2,vector<int> (2,-1)));
+        int ans = rec(0,prices,0,dp,0);
+        return ans;
     }
 };
