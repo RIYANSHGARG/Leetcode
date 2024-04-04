9e9edad004356b91bea1c2cd9b83db6745f12c39
Riyansh Garg <riyanshgarg2103@gmail.com>
2024-04-04 14:03:09 +0530

    Time: 0 ms (100.00%), Space: 9.7 MB (93.33%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,129 +11,17 @@
  */
 class Solution {
 public:
-vector<int> v;
+    void rec(TreeNode *root, vector<int> &ans){
+        if(root==NULL)  return ;
+        rec(root->left,ans);
+        ans.push_back(root->val);
+        rec(root->right,ans);
+    }
     vector<int> inorderTraversal(TreeNode* root) {
-        
-        // Method 1 : recursive
-        // if(root==NULL)  return {};
-        // inorderTraversal(root->left);
-        // v.push_back(root->val);
-        // inorderTraversal(root->right);
-        // return v;
-
-        // Methdo 2 : Iterative
-        // if(root==NULL)  return {};
-        // vector<int> ans;
-        // TreeNode * node=root;
-        // stack<TreeNode *> s;
-        // while(true){
-        //     if(node!=NULL){
-        //         s.push(node);
-        //         node=node->left;
-        //     }
-        //     else{
-        //         if(s.empty())  break;
-        //         node=s.top();
-        //         s.pop();
-        //         ans.push_back(node->val);
-        //         node=node->right;
-        //     }
-        // }
-        // return ans;
-
-        // Method 3 : Finding all pre in and post order in go using 1 stack
-        // if(root==NULL)  return {};
-        // stack<pair<TreeNode *,int>> s;
-        // vector<int> pre,in,post;
-        // s.push({root,1});
-        // while(!s.empty()){
-        //     auto it=s.top();
-        //     s.pop();
-        //     if(it.second==1){
-        //         pre.push_back(it.first->val);
-        //         it.second++;
-        //         s.push(it);
-        //         if(it.first->left!=NULL)    s.push({it.first->left,1});
-        //     }
-        //     else if(it.second==2){
-        //         in.push_back(it.first->val);
-        //         it.second++;
-        //         s.push(it);
-        //         if(it.first->right!=NULL)   s.push({it.first->right,1});
-        //     }
-        //     else{
-        //         post.push_back(it.first->val);
-        //     }
-        // }
-        // return in;
-
-        // Method 4 : Morris Traversal
-        // vector<int> in;
-        // TreeNode *cur=root;
-        // while(cur){
-        //     if(!cur->left){
-        //         in.push_back(cur->val);
-        //         cur=cur->right;
-        //     }else{
-        //         TreeNode *prev=cur->left;
-        //         while(prev->right && prev->right!=cur)    prev=prev->right;
-        //         if(!prev->right)    {prev->right=cur;cur=cur->left;}
-        //         else{prev->right=NULL;in.push_back(cur->val);cur=cur->right;}
-        //     }
-        // }
-        // return in;
-        
-        
-        // practice :
-        //  recursion         
-        // if(!root)   return {};
-        // inorderTraversal(root->left);
-        // v.push_back(root->val);
-        // inorderTraversal(root->right);
-        // return v;
-        
-        //  iterative
-        // TreeNode *node = root;
-        // stack<TreeNode *> s;
-        // vector<int> in;
-        // while(true){
-        //     if(node){
-        //         s.push(node);
-        //         node=node->left;
-        //     }else{
-        //         if(s.empty())   break;
-        //         node = s.top();
-        //         s.pop();
-        //         in.push_back(node->val);
-        //         node=node->right;
-        //     }
-        // }
-        // return in;
-        
-        //  All in one stack
-        if(root==NULL)  return {};
-        stack<pair<TreeNode *,int>> s;
-        vector<int> pre,in,post;
-        
-        s.push({root,1});
-        while(!s.empty()){
-            TreeNode *node = s.top().first;
-            int l=s.top().second;
-            s.pop();
-            if(l==1){
-                pre.push_back(node->val);
-                s.push({node,2});
-                if(node->left)  s.push({node->left,1});
-            }
-            else if(l==2){
-                in.push_back(node->val);
-                s.push({node,3});
-                if(node->right) s.push({node->right,1});
-            }else{
-                post.push_back(node->val);
-            }
-        }
-        return in;
+        vector<int> ans;
+        rec(root,ans);
+        return ans;
+      
         
     }
 };
