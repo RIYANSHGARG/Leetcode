71cb4d4e3e9328ea826df35290288fb8500d7f2c
Riyansh Garg <riyanshgarg2103@gmail.com>
2024-04-06 19:09:12 +0530

    Time: 57 ms (16.54%), Space: 47.8 MB (18.89%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,20 +1,21 @@
 class Solution {
-    
 public:
-    long long rec(int i, int amount , vector<int> &coins,vector<vector<long long>> &dp){
+    
+    long long int rec(int i,int amount,vector<int> &amounts,vector<vector<int>> &dp){
         if(amount==0)   return 0;
         if(i<0 || amount<0) return INT_MAX;
         
         if(dp[i][amount]!=-1)   return dp[i][amount];
         
-        long long notPick = rec(i-1,amount,coins,dp);
-        long long pick = 1+rec(i,amount-coins[i],coins,dp);
+        long long int pick=1+rec(i,amount-amounts[i],amounts,dp);
+        long long int notPick=rec(i-1,amount,amounts,dp);
         
-        return dp[i][amount] = min(pick,notPick);
+        return dp[i][amount]=min(pick,notPick);
     }
+    
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<long long>> dp(coins.size()+1 , vector<long long>(amount+1,-1));
-        long long ans = rec(coins.size()-1 , amount , coins,dp);
+        vector<vector<int>> dp(coins.size(),vector<int> (amount+1,-1));
+        long long int ans = rec(coins.size()-1,amount,coins,dp);
         return (ans<INT_MAX)?ans:-1;
     }
 };
