a9a984f9bc9ab4d90e546c0afb32bf3a037aa661
Riyansh Garg <riyanshgarg2103@gmail.com>
2024-04-19 14:30:26 +0530

    Time: 2 ms (42.85%), Space: 9.8 MB (45.78%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -19,23 +19,49 @@ public:
         rec(root->right,ans);
     }
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> ans;
+        // vector<int> ans;
         // rec(root,ans);
         
         // root left right
-        if(!root)   return {};
-        stack<TreeNode  *> st;
-        st.push(root);
-        while(!st.empty()){
-            TreeNode *temp=st.top();
-            st.pop();
-            ans.push_back(temp->val);
+//         if(!root)   return {};
+//         stack<TreeNode  *> st;
+//         st.push(root);
+//         while(!st.empty()){
+//             TreeNode *temp=st.top();
+//             st.pop();
+//             ans.push_back(temp->val);
             
-            if(temp->right) st.push(temp->right);
-            if(temp->left)  st.push(temp->left);
+//             if(temp->right) st.push(temp->right);
+//             if(temp->left)  st.push(temp->left);
+//         }
+        
+        
+//         return ans;
+        
+        if(!root)   return {};
+        vector<int> ans;
+        TreeNode *cur = root;
+        while(cur!=NULL){
+            if(!cur->left){
+                ans.push_back(cur->val);
+                cur=cur->right;
+            }else{
+                TreeNode *prev = cur->left;
+                while(prev->right && prev->right!=cur){
+                    prev=prev->right;
+                }
+                if(prev->right==NULL){
+                    prev->right=cur;
+                    ans.push_back(cur->val);
+                    cur=cur->left;
+                }else{
+                    
+                    prev->right=NULL;
+                    
+                    cur=cur->right;
+                }
+            }
         }
-        
-        
         return ans;
     }
 };
