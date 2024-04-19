f5927523e6c3c2927147451df1a5ff882ac44377
Riyansh Garg <riyanshgarg2103@gmail.com>
2024-04-19 13:48:37 +0530

    Time: 5 ms (8.96%), Space: 9.8 MB (78.67%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -20,7 +20,22 @@ public:
     }
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> ans;
-        rec(root,ans);
+        // rec(root,ans);
+        
+        // root left right
+        if(!root)   return {};
+        stack<TreeNode  *> st;
+        st.push(root);
+        while(!st.empty()){
+            TreeNode *temp=st.top();
+            st.pop();
+            ans.push_back(temp->val);
+            
+            if(temp->right) st.push(temp->right);
+            if(temp->left)  st.push(temp->left);
+        }
+        
+        
         return ans;
     }
 };
