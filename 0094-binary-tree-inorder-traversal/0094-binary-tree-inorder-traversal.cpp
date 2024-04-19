6e76281e4d14709f756e9b8143a31a5a16de27e2
Riyansh Garg <riyanshgarg2103@gmail.com>
2024-04-19 14:18:06 +0530

    Time: 0 ms (100.00%), Space: 9.7 MB (72.50%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -23,20 +23,46 @@ public:
         // return ans;
       
         // iteratively
+        // if(!root)   return {};
+        // TreeNode *node=root;
+        // stack<TreeNode *> st;
+        
+        // vector<int> ans;
+        
+        // while(true){
+        //     if(node){st.push(node);node=node->left;}
+        //     else{
+        //         if(st.empty())  break;
+        //         node=st.top();
+        //         st.pop();
+        //         ans.push_back(node->val);
+        //         node=node->right;
+        //     }
+        // }
+        // return ans;
+        
+        
+//          Morris traversal
         if(!root)   return {};
-        TreeNode *node=root;
-        stack<TreeNode *> st;
-        
         vector<int> ans;
-        
-        while(true){
-            if(node){st.push(node);node=node->left;}
-            else{
-                if(st.empty())  break;
-                node=st.top();
-                st.pop();
-                ans.push_back(node->val);
-                node=node->right;
+        TreeNode *cur = root;
+        while(cur!=NULL){
+            if(!cur->left){
+                ans.push_back(cur->val);
+                cur=cur->right;
+            }else{
+                TreeNode *prev = cur->left;
+                while(prev->right && prev->right!=cur){
+                    prev=prev->right;
+                }
+                if(prev->right==NULL){
+                    prev->right=cur;
+                    cur=cur->left;
+                }else{
+                    prev->right=NULL;
+                    ans.push_back(cur->val);
+                    cur=cur->right;
+                }
             }
         }
         return ans;
